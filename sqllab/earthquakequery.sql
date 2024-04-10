df32c8418aff38be35520b8bd36cff7cb0b1d9e0
DanielChen29 <daniel.yp.chen@gmail.com>
2024-04-10 10:31:27 -0500

    created  SQL queries

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -1,1 +1,8 @@
 -- Find the 10 highest magnitude earthquakes
+SELECT * FROM earthquakes ORDER BY magnitude DESC LIMIT 10;
+
+-- Find all earthquakes that occurred on January 27th in Hawaii
+SELECT * FROM earthquakes WHERE DATE(quaketime) = '2023-01-27' AND place LIKE '%Hawaii%';
+
+-- Find all earthquakes below the equator
+SELECT * FROM earthquakes WHERE latitude < 0;
