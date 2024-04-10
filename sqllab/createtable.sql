700545c4eb7c4b2e2f80e7a89852964150e71ae3
DanielChen29 <daniel.yp.chen@gmail.com>
2024-04-09 21:50:46 -0500

    changed sql scheme

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
+  quaketime timestamp,
   latitude real,
   longitude real,
   mag real,
