2da0fbad94a307074904fa53b84ed00d451593d6
DanielChen29 <daniel.yp.chen@gmail.com>
2024-04-15 13:02:14 -0500

    added create table methods

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,14 +1,36 @@
-DROP TABLE IF EXISTS cities;
-DROP TABLE IF EXISTS states;
-CREATE TABLE cities (
-  City text,
-  State text,
-  Population real,
-  Latitude real,
-  Longitude real
-);
-CREATE TABLE States (
-  Code text,
-  State text,
-  Population real
-);
+import psycopg2
+
+def create_tables():
+    """ Create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE cities (
+            City text,
+            State text,
+            Population real,
+            Latitude real,
+            Longitude real
+        )
+        """,
+        """ 
+        CREATE TABLE States (
+                Code text,
+                State text,
+                Population real
+        )
+        """)
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="chend2",
+        user="chend2",
+        password="plad242books")
+
+    cur = conn.cursor()
+
+    for command in commands:
+        cur.execute(command)
+
+if __name__ == '__main__':
+    create_tables()
