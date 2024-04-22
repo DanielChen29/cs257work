79ea46a09746eea392a8e932925559f648f5d10d
DanielChen29 <daniel.yp.chen@gmail.com>
2024-04-22 10:38:25 -0500

    added to flasklab

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -16,6 +16,11 @@ def my_display(word1, word2):
 def my_color(word1):
     return '<h1 style="color:Green">' + word1 + '</h1>'
 
+@app.route('/add/<int1>/<int2>')
+def my_display(int1, int2):
+    the_string = int1 + " + " + int2 + " = " + (int1 + int2);
+    return the_string
+
 if __name__ == '__main__':
     my_port = 5202
     app.run(host='0.0.0.0', port = my_port) 
