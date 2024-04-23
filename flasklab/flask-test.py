b91b04980801997e13ea949f8f7276197f7127ac
DanielChen29 <daniel.yp.chen@gmail.com>
2024-04-23 10:51:10 -0500

    added to flasklan

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -3,25 +3,29 @@ import psycopg2
 
 app = flask.Flask(__name__)
 
-#
+# Display html hello world
 @app.route('/hello')
 def my_function():
     return "Hello World!"
 
+# Display two words specified in the html
 @app.route('/display/<word1>/<word2>')
 def my_display(word1, word2):
     the_string = "The words are: " + word1 + " and " + word2;
     return the_string
 
+# Display a word in green specified by the html
 @app.route('/color/<word1>')
 def my_color(word1):
     return '<h1 style="color:Green">' + word1 + '</h1>'
 
+# Add two integers in the html
 @app.route('/add/<int1>/<int2>')
 def my_add(int1, int2):
     the_string = int1 + " + " + int2 + " = " + str((int(int1) + int(int2)));
     return the_string
 
+# Return the population of a state specified in the html
 @app.route('/pop/<abbrev>')
 def my_pop(abbrev):
     state = abbrev
